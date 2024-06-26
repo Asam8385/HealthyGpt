fe994e2db5d43bd554c4842358380a304c306e50
Asam <subairasam8733260@gmail.com>
2024-06-26 19:36:06 +0530

    finally done

--- MedicalApp-rusham/src/server.js
+++ MedicalApp-rusham/src/server.js
@@ -3,16 +3,15 @@ const cors = require('cors');
 const chatRouter = require('./routes/chat');
 
 const app = express();
-const port = 3000;
+
 app.use(cors());
 app.use(express.json());
 
 
 app.use('/chat', chatRouter);
 
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
 });
 
 module.exports = app;
